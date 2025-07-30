31c43eeed25ecc247fd49a09dbdb47da48dbb842
쑤아 <kimsua77@naver.com>
2025-07-30 14:05:08 +0900

    게시판 딕테일 추가

--- 따릉이_web/bicycle_sungdong/lib/view/post_detail.dart
+++ 따릉이_web/bicycle_sungdong/lib/view/post_detail.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:bicycle_sungdong/model/gesigle.dart';
 
-class PostDetail extends StatelessWidget {
-  const PostDetail({super.key});
+class GesigleDetailPage extends StatelessWidget {
+  final Gesigle post;
+  const GesigleDetailPage({required this.post, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text(post.title)),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('제목: ${post.title}', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 16),
+            Text('작성일: ${post.datetime}', style: TextStyle(color: Colors.grey)),
+            SizedBox(height: 32),
+            Text(post.content, style: TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
     );
   }
 }
